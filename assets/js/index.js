540407b1c394f66745aa1046d92174c8fac1f39a
Antonio Banegas <cae1814@gmail.com>
2024-07-20 19:52:57 -0600

    Final
    Finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,52 @@ main();
 
 function main() {
      // Agregando evento al boton Limpiar 
-    btonLimpiar.addEventListener("click", limpiar);
+     eje1_btonLimpiar.addEventListener("click", eje1_limpiar);
 
      // Agregando evento al boton Calcular 
-     btonCalcular.addEventListener("click", calcular);
+     eje1_btonCalcular.addEventListener("click",eje1_calcular);
 }
 
-function limpiar() {
-    document.getElementById("inputBase").value = 0;
-    document.getElementById("inputAltura").value = 0;
+function eje1_limpiar() {
+    document.getElementById("eje1InputBase").value = 0;
+    document.getElementById("eje1InputAltura").value = 0;
 }
 
-function calcular() {
-    let b = parseInt(document.getElementById("inputBase").value);
-    let a = parseInt(document.getElementById("inputAltura").value);
-    document.getElementById("inputArea").value = b * a;
+function eje1_calcular() {
+    document.getElementById("eje1InputArea").value = "";
+    let base = document.getElementById("eje1InputBase").value;
+    let altura = document.getElementById("eje1InputAltura").value;
+
+    if (eje1_validaciones(altura, base)) {
+        document.getElementById("eje1InputArea").value = parseInt(base) * parseInt(altura);
+    } else {
+       //alert("Los datos ingresados son incorrectos");
+    }
+}
+
+function eje1_validaciones(pa, pb){
+    if (!isNumber(pa) || pa.length == 0 ) {
+        
+        document.getElementById("error-eje1InputAltura").removeAttribute('hidden');
+        return false;
+    } else {
+        document.getElementById("error-eje1InputAltura").setAttribute('hidden', '');
+    }
+    
+    if (!isNumber(pb) || pb.length == 0 ) {
+        document.getElementById("error-eje1InputBase").removeAttribute('hidden');
+        return false;
+    } else {
+        document.getElementById("error-eje1InputBase").setAttribute('hidden', '');
+    }
+
+    return true;
+};
+
+function isNumber(pnum) {
+    if (isNaN(pnum)) {
+        return false;
+    } else {
+        return true;
+    }
 }
